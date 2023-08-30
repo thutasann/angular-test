a81391c8e3ce733781864d82683f3f589d210d3f
Thuta San <thutasann2002@gmail.com>
2023-08-30 18:55:34 +0630

    UI: responsive layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core'
+import { BreakpointObserver } from '@angular/cdk/layout'
+import { ChangeDetectorRef, Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core'
 })
 export class AppComponent {
   title = 'genesiv-test'
+
+  hide: boolean = true
+
+  constructor(
+    private observer: BreakpointObserver,
+    private dir: ChangeDetectorRef
+  ) {}
+
+  ngAfterViewInit(): void {
+    this.hide = true
+    this.observer.observe(['(max-width:800px)']).subscribe(res => {
+      if (res?.matches) {
+        this.hide = true
+      } else {
+        this.hide = false
+      }
+    })
+    this.dir.detectChanges()
+  }
 }
