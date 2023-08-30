77c115b4f854525c774e94960533bd3a6ba6e8e2
Thuta San <thutasann2002@gmail.com>
2023-08-30 14:25:30 +0630

    UI: responsive side navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core'
+import { BreakpointObserver } from '@angular/cdk/layout'
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
+  isSidebarOpen = false
 
+  constructor(
+    private observer: BreakpointObserver,
+    private dir: ChangeDetectorRef
+  ) {}
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen
+  }
+
+  ngAfterViewInit(): void {
+    this.isSidebarOpen = true
+    this.observer.observe(['(max-width:800px)']).subscribe(res => {
+      if (res?.matches) {
+        this.isSidebarOpen = true
+      } else {
+        this.isSidebarOpen = false
+      }
+    })
+    this.dir.detectChanges()
+  }
 }
