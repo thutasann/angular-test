d3a219f50ac0e9190841322e0184172572c65805
Thuta San <thutasann2002@gmail.com>
2023-08-30 17:22:00 +0630

    chore: optimize image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,26 @@ import { BrowserModule } from '@angular/platform-browser'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { NavbarComponent } from './navbar/navbar.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component'
+import { SidebarComponent } from './sidebar/sidebar.component'
 import { SidebarItemComponent } from './sidebar/sidebar-item/sidebar-item.component'
+import { MessagesComponent } from './messages/messages.component'
+import { NgOptimizedImage } from '@angular/common'
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent, SidebarComponent, SidebarItemComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    SidebarItemComponent,
+    MessagesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgOptimizedImage,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
